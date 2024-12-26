f4d9c59e976fd81d3262c4da9f8ddaa749729ff6
kumar.sanjay <kumar.sanjay@gmail.com>
2024-12-26 01:09:22 +0000

    changes

--- .idx/main.go
+++ .idx/main.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/chromedp/chromedp"
+)
 
 func sum(i, j int) int {
 	return i + j
 }
 
 func main() {
-	i := 0
-	j := 2
-	i = i + 1
-	fmt.Println("hello world 3")
-	fmt.Println(i)
-	k := sum(i, j)
-	fmt.Println(k)
+	fmt.Println("go")
+	fmt.Println(sum(1, 2))
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://www.google.com`),
+		//		chromedp.WaitVisible(`body > div.L3eUgb > div.o3j9`),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("go end")
 }
